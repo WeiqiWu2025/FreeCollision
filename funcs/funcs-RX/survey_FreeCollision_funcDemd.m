584acb1e9083310514522c6877fe45d435a963ac
WeiqiWu2025 <weiqiwu@ustc.edu.cn>
2026-02-03 21:27:58 +0800

    Delete funcs directory

--- funcs/funcs-RX/survey_FreeCollision_funcDemd.m
+++
@@ -1,9 +0,0 @@
-function [demodData] = survey_FreeCollision_funcDemd(estBaseH,numTagData,Q,numTags)
-
-% demodData = zeros(numTagData,numTags);
-
-possiValue = survey_FreeCollision_funcPossibleValues(numTags);
-demodData = survey_FreeCollision_funcMLDetector(possiValue,estBaseH,Q,numTagData,numTags);
-
-end
-
--- funcs/funcs-RX/survey_FreeCollision_funcSymbolClustering.m
+++
@@ -1,8 +0,0 @@
-function [clusterLabels,clusterCenters] = survey_FreeCollision_funcSymbolClustering(clusterPoints,clusterNumber)
-
-tmp = [real(clusterPoints);imag(clusterPoints)].';
-
-[clusterLabels,clusterCenters] = kmeans(tmp,clusterNumber,'Distance','cityblock','MaxIter',10000,'Replicates',100); 
-
-end
-
